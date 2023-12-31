3a49d646af311b202b3d5ae3af3a5bde01e995e2
HAMMADMIRFAN <DEV.HAMMAD1@GMAIL.COM>
2023-12-31 02:04:19 -0800

    nodemon

--- Nodejs/static.js
+++ Nodejs/static.js
@@ -1,2 +1,16 @@
 const express = require("express");
 const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+// static folder
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+    res.send("Express Works");
+    // res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(app.get("port"), function () {
+    console.log(`Express Started on : http://localhost:${app.get("port")}`);
+});
